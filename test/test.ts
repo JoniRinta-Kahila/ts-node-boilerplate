3ca32452dc3293c7144fca8762c75fd088e2e2f0
JoniRinta-Kahila <joni.rintakahila@opp.eduvantaa.fi>
2021-08-26 13:49:18 +0300

    created tests

---
+++ test/test.ts
@@ -0,0 +1,9 @@
+import { expect } from 'chai';
+import 'mocha';
+
+describe('Initial test', () => {
+  it('checking the tests are working', () => {
+    const val = 1;
+    expect(val).to.equal(1);
+  });
+});
